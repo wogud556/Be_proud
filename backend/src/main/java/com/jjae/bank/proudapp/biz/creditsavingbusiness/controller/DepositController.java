5b6c84d8e4cb846a3fc9ab1afcf004e37c7f2c71
wogud556 <wogud556@naver.com>
2022-07-16 16:54:57 +0900

    start java

--- backend/src/main/java/com/jjae/bank/proudapp/biz/creditsavingbusiness/controller/DepositController.java
+++ backend/src/main/java/com/jjae/bank/proudapp/biz/creditsavingbusiness/controller/DepositController.java
@@ -1,8 +1,23 @@
 package com.jjae.bank.proudapp.biz.creditsavingbusiness.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /*
 * 예금 업무에서 수행하는 controller
 *
 * */
+@RestController
+@RequestMapping(value = "/CreditSave/Deposit")
+
 public class DepositController {
+
+    @GetMapping(value = "/helloworld")
+    public ResponseEntity helloworld(){
+        System.out.println("call success");
+        return ResponseEntity.ok("안녕하세요");
+    }
+
 }
--- backend/src/main/java/com/jjae/bank/proudapp/biz/creditsavingbusiness/controller/UserInformController.java
+++ backend/src/main/java/com/jjae/bank/proudapp/biz/creditsavingbusiness/controller/UserInformController.java
@@ -1,8 +1,20 @@
 package com.jjae.bank.proudapp.biz.creditsavingbusiness.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /*
 * 사용자의 개인정보 입력 삭제 등을 수행하는 controller
 *
 * */
+@RestController
+@RequestMapping(value = "/CreditSave/UserInform")
 public class UserInformController {
+    @GetMapping(value = "/helloworld3")
+    public ResponseEntity helloworld(){
+        System.out.println("call success");
+        return ResponseEntity.ok("안녕하세요");
+    }
 }
